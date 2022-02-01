53b50862a90673f5d9eca0807ab2611073215e22
max <mpavlov@mrcentertainment.com>
2022-02-01 23:16:51 +0200

    integrated nest as an mvc js framework. Sorry java :)

---
+++ src-js/mvc/src/app.controller.ts
@@ -0,0 +1,38 @@
+import {Get, Controller, Render} from '@nestjs/common';
+
+const CloudIpsp = require('cloudipsp-node-js-sdk')
+
+
+@Controller()
+export class AppController {
+    @Get()
+    @Render('index')
+    async root() {
+        let result;
+        const fondy = new CloudIpsp(
+            {
+                merchantId: '1494220',
+                secretKey: 'Z8YwcMnR6ad82Zv9UmQPH7R5HID7L2Zc'
+            }
+        )
+
+        const requestData = {
+            order_id: new Date().getTime(),
+            order_desc: 'test order',
+            currency: 'UAH',
+            amount: '100'
+        }
+
+       await fondy.Checkout(requestData).then(data => {
+            console.log(data)
+            result = data
+        }).catch((error) => {
+            console.log(error)
+            result = error
+        })
+
+        return {
+            message: result.checkout_url
+        };
+    }
+}
