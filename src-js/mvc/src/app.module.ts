83deaaedcef90a645795b446f5f99d3f76340351
max <mpavlov@mrcentertainment.com>
2022-02-09 22:40:24 +0200

    added landing

--- src-js/mvc/src/app.module.ts
+++ src-js/mvc/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChargeController } from './charge.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ChargeController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static-files'),   // <-- path to the static files
+    }),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
