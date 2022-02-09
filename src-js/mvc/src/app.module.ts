5e2610a9b2fa73533f92133267a75fea095176f3
max <mpavlov@mrcentertainment.com>
2022-02-09 22:58:35 +0200

    fixed charge

--- src-js/mvc/src/app.module.ts
+++ src-js/mvc/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import {ChargeController} from "./charge.controller";
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -10,7 +11,7 @@ import { join } from 'path';
       rootPath: join(__dirname, '..', 'static-files'),   // <-- path to the static files
     }),
   ],
-  controllers: [AppController],
+  controllers: [AppController, ChargeController],
   providers: [AppService],
 })
 export class AppModule {}
