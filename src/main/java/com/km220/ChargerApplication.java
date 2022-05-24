5b090b3e571f2d6a8de901ebeb7567a96f842fa9
vdvoretskyi <jasper.ua@gmail.com>
2022-05-24 09:15:45 +0300

    upgrade to spring boot 2.7.0 upgrade to java 17

--- src/main/java/com/km220/ChargerApplication.java
+++ src/main/java/com/km220/ChargerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChargerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChargerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ChargerApplication.class, args);
+  }
 
 }
