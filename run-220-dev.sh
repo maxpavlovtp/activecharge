331044a8ec6fb316f00cbb00d73f74cadd6a4832
max <mpavlov@mrcentertainment.com>
2022-05-25 21:44:07 +0300

    220-40 dev runner

--- run-220-dev.sh
+++ run-220-dev.sh
@@ -1,7 +1,11 @@
 cd "$(dirname "$0")" || exit
 
 pkill -f react
+
 pkill -f nest
+pkill -f java
+
+nohup java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar &
+nohup ./src-js/nest/run-nest-dev.sh &
 
 nohup ./src-js/react/run-react-dev.sh &
-nohup ./src-js/nest/run-nest-dev.sh &
--- run-220-java-prod.sh
+++
@@ -1,4 +0,0 @@
-cd "$(dirname "$0")" || exit
-
-pkill -f java
-nohup java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar &
