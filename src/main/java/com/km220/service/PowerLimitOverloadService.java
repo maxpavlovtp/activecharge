5dde2f508a28ec6d0382a7290b308b91a6575023
max <mpavlov@mrcentertainment.com>
2022-06-03 23:54:08 +0300

    220-48 8 hours

--- src/main/java/com/km220/service/PowerLimitOverloadService.java
+++ src/main/java/com/km220/service/PowerLimitOverloadService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class PowerLimitOverloadService {
 
   //todo move to db
-  public static final int OVERLOAD_LIMIT_TIMER_SECS = 20;
+  public static final int OVERLOAD_LIMIT_TIMER_SECS = 3600 * 8;
   private static final int POWER_LIMIT_WTH = 1900;
 
   public boolean isPowerLimitOvelrloaded() {
