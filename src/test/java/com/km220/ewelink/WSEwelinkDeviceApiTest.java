e38920bf29181b72befe4d6a6a59d975dce267b0
vdvoretskyi <jasper.ua@gmail.com>
2022-06-08 10:16:21 +0300

    add ewelink-java-websocket-client

--- src/test/java/com/km220/ewelink/WSEwelinkDeviceApiTest.java
+++ src/test/java/com/km220/ewelink/WSEwelinkDeviceApiTest.java
@@ -1,9 +1,11 @@
 package com.km220.ewelink;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.km220.ewelink.model.ws.WssResponse;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,27 +30,35 @@ class WSEwelinkDeviceApiTest {
 
   @Test
   void getDeviceShouldReturnDevice() throws InterruptedException {
-    var response = new AtomicReference<WssResponse>();
-
-    var latch = new CountDownLatch(1);
+    BlockingQueue<WssResponse> messageQueue = new LinkedBlockingQueue<>();
+    AtomicReference<Throwable> errorRef = new AtomicReference<>();
     WSClientListener listener = new WSClientListener() {
 
       @Override
       public void onMessage(final WssResponse message) {
-        response.set(message);
-        latch.countDown();
+        messageQueue.add(message);
       }
 
       @Override
       public void onError(final Throwable error) {
-        latch.countDown();
+        error.printStackTrace();
+        errorRef.set(error);
       }
     };
 
     WSEwelinkDeviceApi api = ewelinkClient.devices(listener);
-    api.getDeviceStatus(DEVICE_ID);
-    latch.await(5, TimeUnit.SECONDS);
 
-    assertNotNull(response.get());
+    for (int i = 0; i < 10000; i++) {
+      api.getDeviceStatus(DEVICE_ID);
+
+      WssResponse response = messageQueue.poll(5, TimeUnit.SECONDS);
+      assertNotNull(response);
+
+      System.out.println(response.getParams());
+
+      assertNull(errorRef.get());
+
+      Thread.sleep(1000);
+    }
   }
 }
