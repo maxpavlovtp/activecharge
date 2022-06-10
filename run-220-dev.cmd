68adef7ff6b060373b3d28d2d52cb105363acf43
dimashechka <dimashechka0213@gmail.com>
2022-06-10 18:55:52 +0300

    220-59 try fix overload

--- run-220-dev.cmd
+++ run-220-dev.cmd
@@ -1,4 +1,4 @@
-taskkill /f /im java.exe & ^
+taskkill /f /im java & ^
 
 gradlew clean build -x test
 java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
--- src/main/java/com/km220/service/PowerLimitOverloadService.java
+++ src/main/java/com/km220/service/PowerLimitOverloadService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class PowerLimitOverloadService {
 
   //todo move to db
-  public static final int OVERLOAD_LIMIT_TIMER_SECS = 10;
+  public static final int OVERLOAD_LIMIT_TIMER_SECS = 30;
   private static final int POWER_LIMIT_WTH = 1900;
 
   public boolean isPowerLimitOvelrloaded() {
