1e8711f44878d8358d5fa40ccd616175a458f632
dimashechka <dimashechka0213@gmail.com>
2022-06-17 14:40:27 +0300

    220-65 can`t go to the main page from other tabs when divice on

--- src-js/react/src/components/privateRoute/PrivateRoute.tsx
+++ src-js/react/src/components/privateRoute/PrivateRoute.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useAppSelector } from "../../hooks/reduxHooks";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { getDeviceIsOnStatus } from "../../store/reducers/ActionCreators";
+import Spinner from "../spinner/Spinner";
 
 export default function PrivateRoute({ children }: { children: any }) {
+  const [loading, setLoading] = useState<boolean>(true);
   const { isDeviceOn } = useAppSelector((state) => state.fetchReducer);
-  console.log('isDeviceOn: ' + isDeviceOn);
-  return isDeviceOn === false ? children : <Link to='/charging' />
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getDeviceIsOnStatus());
+    setLoading(false);
+  }, [dispatch]);
+
+  if (loading) return <Spinner />;
+  return !isDeviceOn ? children : <Navigate to="/charging" />;
 }
