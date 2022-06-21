3bc8d55b4c1b31cb69b0a0db3a54fbbe3211a63a
dimashechka <dimashechka0213@gmail.com>
2022-06-21 12:50:04 +0300

    220-87 fix rendering kwt charged

--- src-js/react/src/components/getPower/GetPower.tsx
+++ src-js/react/src/components/getPower/GetPower.tsx
@@ -37,8 +37,10 @@ export default function GetPower() {
   // tesla model 3 = 100
   let carKwtKmRatio = 150;
   let isZero = chargingStatus === undefined;
-  let chargeStatus = `${isZero ? " " : kWtCharged} ${t("wt")}  (${t("around")} ${
-    isZero ? 0 : Math.round(kWtCharged * 1000 / carKwtKmRatio)
+  let chargeStatus = `${isZero ? " " : kWtCharged.toFixed(2)} ${t("wt")}  (${t(
+    "around"
+  )} ${
+    isZero ? 0 : Math.round((kWtCharged * 1000) / carKwtKmRatio).toFixed(2)
   } km)`;
 
   return (
