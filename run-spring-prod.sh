35f6e70edbab98b93edd54eb8faf8bcce763df94
vdvoretskyi <jasper.ua@gmail.com>
2022-07-07 11:56:48 +0300

    profiles, db init, migrations.

--- run-spring-prod.sh
+++ run-spring-prod.sh
@@ -4,4 +4,4 @@ export JAVA_HOME="/opt/jdk-17"
 export PATH=$JAVA_HOME/bin:$PATH
 
 ./gradlew clean build -x test
-java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=prod
+java -Dspring.profiles.active=prod -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
