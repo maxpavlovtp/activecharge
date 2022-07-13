ae16ab31696812a85e6971705462a948fdf4c63d
max <mpavlov@mrcentertainment.com>
2022-07-13 16:47:38 +0300

    220-97 postgress setup

---
+++ run-220-local.sh
@@ -0,0 +1,19 @@
+cd "$(dirname "$0")" || exit
+
+pkill -f java
+pkill -f react
+pkill -f nest
+
+lsof -ti tcp:8080 | xargs kill -kill
+lsof -ti tcp:5000 | xargs kill -kill
+lsof -ti tcp:3000 | xargs kill -kill
+
+nohup ./src-js/nest/run-nest-dev.sh &
+nohup ./src-js/react/run-react-dev.sh &
+
+#docker-compose down && rm -r ./db-data
+#docker compose up
+./gradlew clean build -x test
+java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=local
+
+#tail -f nohup.out
--- run-spring-prod.sh
+++
@@ -1,7 +0,0 @@
-cd "$(dirname "$0")" || exit
-
-export JAVA_HOME="/opt/jdk-17"
-export PATH=$JAVA_HOME/bin:$PATH
-
-./gradlew clean build -x test
-java -Dspring.profiles.active=prod -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
