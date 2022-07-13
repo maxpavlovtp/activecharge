a6def2259cb9f68b650b145683bcf899da450f70
max <mpavlov@mrcentertainment.com>
2022-07-13 23:22:36 +0300

    220-97 postgress setup

--- run-220-localWithDevDB.sh
+++ run-220-localWithDevDB.sh
@@ -14,6 +14,6 @@ nohup ./src-js/react/run-react-dev.sh &
 #docker-compose down && rm -r ./db-data
 #docker compose up
 ./gradlew clean build -x test
-java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=dev
+java -Dspring.profiles.active=dev -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
 
 #tail -f nohup.out
--- systemctl-run-be-prod.sh
+++ systemctl-run-be-prod.sh
@@ -1,3 +1,3 @@
 cd "$(dirname "$0")" || exit
 
-java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=stage
+java -Dspring.profiles.active=prod -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
