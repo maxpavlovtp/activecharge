5ebf1746f016264226a13f705fbca4a2a45055a3
max <mpavlov@mrcentertainment.com>
2022-07-14 00:39:35 +0300

    220-97 postgress setup

--- run-220-local-debug.sh
+++ run-220-local-debug.sh
@@ -13,6 +13,6 @@ nohup ./src-js/react/run-react-dev.sh &
 
 ./gradlew clean build -x test
 #java -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -Dspring.profiles.active=dev -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=local -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
 
 #tail -f nohup.out
