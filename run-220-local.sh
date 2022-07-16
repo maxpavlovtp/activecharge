b84fcba97c4dab86fc8a2f1d83944db76c7c78df
max <mpavlov@mrcentertainment.com>
2022-07-16 12:06:33 +0300

    220-1 cosmetics

--- run-220-local.sh
+++ run-220-local.sh
@@ -13,6 +13,7 @@ nohup ./src-js/react/run-react-dev.sh &
 
 docker-compose down && rm -r ./db-data
 nohup docker compose up &
+echo "DB in docker is starting..."
 sleep 50s
 ./gradlew clean build -x test
 java -Dspring.profiles.active=local -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
