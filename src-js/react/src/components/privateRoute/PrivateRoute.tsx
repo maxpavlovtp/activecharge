dedcc76d3d23a03415b055195c629b54283ead71
dimaborbotko <dimashechka0213@gmail.com>
2022-07-26 13:10:03 +0300

    220-128 fix url when charging

--- src-js/react/src/components/privateRoute/PrivateRoute.tsx
+++ src-js/react/src/components/privateRoute/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useSearchParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { useEffect } from "react";
 import { getStationInfo } from "../../store/reducers/ActionCreators";
@@ -7,10 +7,14 @@ export default function PrivateRoute({ children }: { children: any }) {
   const dispatch = useAppDispatch();
   const { deviceStatus } = useAppSelector((state) => state.fetchReducer);
 
+  const [searchParams] = useSearchParams();
+  let stationNumbers: any = searchParams.get("station");
+  localStorage.setItem("stationNumber", stationNumbers ? stationNumbers : "2");
+
   useEffect(() => {
     dispatch(getStationInfo());
     console.log(deviceStatus?.state)
   }, []);
 
-  return deviceStatus?.state === "IN_PROGRESS" ? <Navigate to="/charging" /> : children;
+  return deviceStatus?.state === "IN_PROGRESS" ? <Navigate to={`/charging?station=${stationNumbers}`}/> : children;
 }
