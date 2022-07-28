180d7e3b758940196eacd9f1b9a8e44d8c183f2e
max <mpavlov@mrcentertainment.com>
2022-07-28 20:15:51 +0300

    220-118 flyway

--- run-220-local-vlad.sh
+++ run-220-local-vlad.sh
@@ -9,15 +9,14 @@ lsof -ti tcp:8080 | xargs kill -kill
 lsof -ti tcp:5000 | xargs kill -kill
 lsof -ti tcp:3000 | xargs kill -kill
 
-docker-compose down && rm -rf ./db-data
-nohup docker-compose up &
-./gradlew clean build -x test
-
+#FE
 nohup ./src-js/nest/run-nest-dev.sh &
 nohup ./src-js/react/run-react-dev.sh &
 
-echo "DB in docker is starting..."
-sleep 50s
+#BE
+#docker-compose down && rm -rf ./db-data
+nohup docker-compose up &
+./gradlew clean build -x test
 java -Dspring.profiles.active=vlad -jar build/libs/220-km.com-0.0.1-SNAPSHOT.jar
 
 #tail -f nohup.out 
--- src/main/resources/db/migration/pg/V4__update_stations.sql
+++ src/main/resources/db/migration/pg/V4__update_stations.sql
@@ -3,4 +3,4 @@ INSERT INTO station ("name", "number", provider_device_id)VALUES('dima','1', '10
 INSERT INTO station ("name", "number", provider_device_id)VALUES('220-dev','4', '1001323420');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('220-prod','5', '1001323877');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('vlad','2', '1000d61c41');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('max','3', '100155422a');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('maxTTT','3', '100155422a');
