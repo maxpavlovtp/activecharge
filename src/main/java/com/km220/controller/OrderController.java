112388a596abd0910edff5f5251dd3f4ead2c695
max <mpavlov@mrcentertainment.com>
2022-08-02 20:53:40 +0300

    220-139 call back

--- src/main/java/com/km220/controller/OrderController.java
+++ src/main/java/com/km220/controller/OrderController.java
@@ -1,34 +1,31 @@
 package com.km220.controller;
 
 import com.km220.aquaring.monobank.InvoiceCreator;
-import com.km220.model.ChargingJob;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.io.IOException;
-import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
-
-  @GetMapping("/generateCheckoutLink")
-  public ResponseEntity<String> getStationStatus(
-      @Parameter(description = "Station number") @NotBlank @RequestParam("station_number") String stationNumber)
+  @PostMapping("/generateCheckoutLink")
+  public ResponseEntity<String> generateCheckoutLink(@RequestBody ChargeRequest chargeRequest)
       throws IOException {
-
     return ResponseEntity.status(HttpStatus.OK).body(InvoiceCreator.generateCheckoutLink());
   }
 
+  @PostMapping("/callBackMono")
+  public ResponseEntity<Void> callBackMono(@RequestBody String callBackMono) {
+    log.info("Call back from monobank: {}", callBackMono);
+    return ResponseEntity.status(HttpStatus.OK).body(null);
+  }
 }
