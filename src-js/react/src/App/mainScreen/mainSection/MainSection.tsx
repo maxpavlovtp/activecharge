32fc4f1d9d82c85c59377b6595df66e335879593
dimaborbotko <dimashechka0213@gmail.com>
2022-08-04 12:28:37 +0300

    220-142 add link to start btn

--- src-js/react/src/App/mainScreen/mainSection/MainSection.tsx
+++ src-js/react/src/App/mainScreen/mainSection/MainSection.tsx
@@ -1,22 +1,26 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MainSection.module.css";
 import mainImg from "../../../assets/charging.png";
-import {Link, useSearchParams} from "react-router-dom";
-import {useTranslation} from "react-i18next";
-import {useAppDispatch, useAppSelector} from "../../../hooks/reduxHooks";
-import {idStart} from "../../../store/reducers/ActionCreators";
+import { Link, useSearchParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
+import { idStart } from "../../../store/reducers/ActionCreators";
 import MainImgLoadingLazy from "../../../components/lazyLoading/MainImgLoadingLazy";
 import placehoderSrc from "../../../assets/chargingTiny.png";
 import ErrorPage from "../../../components/error-page/ErrorPage";
-import {setDeviceStatusUndefind} from "../../../store/reducers/FetchSlice";
+import { setDeviceStatusUndefind } from "../../../store/reducers/FetchSlice";
+import axios from "axios";
+import Spinner from "../../../components/spinner/Spinner";
 
 const MainSection: React.FC = () => {
   const [searchParams] = useSearchParams();
+  const [payUrl, setPayUrl] = useState<any>(null)
   let stationNumber: any = searchParams.get("station");
+  const urlPayment = `${process.env.REACT_APP_LINK_SERVE}order/generateCheckoutLink?station_number=${stationNumber}`;
 
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
-  const {error} = useAppSelector((state) => state.fetchReducer);
+  const { error } = useAppSelector((state) => state.fetchReducer);
 
   const dispatch = useAppDispatch();
 
@@ -25,81 +29,97 @@ const MainSection: React.FC = () => {
     dispatch(setDeviceStatusUndefind(undefined));
   };
 
+  useEffect(() => {
+    try {
+      axios.get(urlPayment).then(function (result: any) {
+        setPayUrl(result?.data?.pageUrl)
+        console.log(result.data);
+      });
+    } catch (e: any) {
+      console.log(e.message);
+    }
+  }, []);
+
   if (error) {
     return (
-        <ErrorPage
-            errorHeader={t("errorDevHeader")}
-            errorBody={t("errorDevBody")}
-        />
+      <ErrorPage
+        errorHeader={t("errorDevHeader")}
+        errorBody={t("errorDevBody")}
+      />
     );
   }
 
-  let withStartBtn = <>
-    <div className={styles.mainBox}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>{t("title")}</h1>
-        <div className={styles.btnStart}>
-          {/*<Link*/}
-          {/*    to="/overload"*/}
-          {/*    className={styles.btnPay}*/}
-          {/*    onClick={startCharging}*/}
-          {/*>*/}
-          {/*  {t("btns.start")}*/}
-          {/*</Link>*/}
+  if (payUrl === null) {
+    return <Spinner />
+  }
 
-          <a  className={styles.btnPay} href="https://pay.mbnk.biz/2208024GGyPq5Vj6rgoj" target="_blank" rel="noreferrer">
-            {t("btns.start")}
-          </a>
+  let withStartBtn = (
+    <>
+      <div className={styles.mainBox}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>{t("title")}</h1>
+          <div className={styles.btnStart}>
+            <a
+              className={styles.btnPay}
+              href={`${payUrl}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {t("btns.start")}
+            </a>
 
-          <Link
+            <Link
               to={`/charging?station=${stationNumber}`}
               className={styles.btn}
               onClick={startCharging}
-          >
-            {t("btns.startFree")}
-          </Link>
-        </div>
-        <div className={styles.imgCont}>
-          <MainImgLoadingLazy
+            >
+              {t("btns.startFree")}
+            </Link>
+          </div>
+          <div className={styles.imgCont}>
+            <MainImgLoadingLazy
               src={mainImg}
               alt={"station"}
               placeholderSrc={placehoderSrc}
               width="256"
               heigth="256"
-          />
+            />
+          </div>
         </div>
       </div>
-    </div>
-  </>
-  let withoutStartBtn = <>
-    <div className={styles.mainBox}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>{t("title")}</h1>
-        <div className={styles.btnStart}>
-          <Link
-              to={`/charging?station=${stationNumber}`}
-              className={styles.btn}
-              onClick={startCharging}
-          >
-            {t("btns.startFree")}
-          </Link>
-        </div>
-        <div className={styles.imgCont}>
-          <MainImgLoadingLazy
-              src={mainImg}
-              alt={"station"}
-              placeholderSrc={placehoderSrc}
-              width="256"
-              heigth="256"
-          />
-        </div>
-      </div>
-    </div>
-  </>
-
-  return (
-      process.env.REACT_APP_LINK_SERVE === "http://220-km.com:8080/"? withoutStartBtn: withStartBtn
+    </>
   );
+  let withoutStartBtn = (
+    <>
+      <div className={styles.mainBox}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>{t("title")}</h1>
+          <div className={styles.btnStart}>
+            <Link
+              to={`/charging?station=${stationNumber}`}
+              className={styles.btn}
+              onClick={startCharging}
+            >
+              {t("btns.startFree")}
+            </Link>
+          </div>
+          <div className={styles.imgCont}>
+            <MainImgLoadingLazy
+              src={mainImg}
+              alt={"station"}
+              placeholderSrc={placehoderSrc}
+              width="256"
+              heigth="256"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
+  return process.env.REACT_APP_LINK_SERVE === "http://220-km.com:8080/"
+    ? withoutStartBtn
+    : withStartBtn;
 };
 
 export default MainSection;
