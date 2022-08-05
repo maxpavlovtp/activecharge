f0776a618817d45b7c8608cf257c236a6fa52114
max <mpavlov@mrcentertainment.com>
2022-08-06 00:01:10 +0300

    220-139 Start with payment. Post fix

--- src/main/java/com/km220/controller/OrderController.java
+++ src/main/java/com/km220/controller/OrderController.java
@@ -3,13 +3,16 @@ package com.km220.controller;
 import com.km220.aquaring.monobank.InvoiceCreator;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.io.IOException;
+import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
-  @PostMapping("/generateCheckoutLink")
-  public ResponseEntity<String> generateCheckoutLink(@RequestBody ChargeRequest chargeRequest)
+
+  @GetMapping("/generateCheckoutLink")
+  public ResponseEntity<String> getStationStatus(
+      @Parameter(description = "Station number") @NotBlank @RequestParam("station_number") String stationNumber)
       throws IOException {
     return ResponseEntity.status(HttpStatus.OK).body(InvoiceCreator.generateCheckoutLink());
   }
