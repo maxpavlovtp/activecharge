221fbec6d2d2613ea6ce84806d0418cd9c62d65e
max <mpavlov@mrcentertainment.com>
2022-08-08 18:50:48 +0300

    220-139 mono token from java -jar -D params

--- src/main/java/com/km220/controller/OrderController.java
+++ src/main/java/com/km220/controller/OrderController.java
@@ -1,11 +1,12 @@
 package com.km220.controller;
 
-import com.km220.aquaring.monobank.InvoiceCreator;
+import com.km220.service.OrderService;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.io.IOException;
 import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
+  @Autowired
+  OrderService orderService;
 
   @GetMapping("/generateCheckoutLink")
   public ResponseEntity<String> getStationStatus(
       @Parameter(description = "Station number") @NotBlank @RequestParam("station_number") String stationNumber)
       throws IOException {
-    return ResponseEntity.status(HttpStatus.OK).body(InvoiceCreator.generateCheckoutLink());
+    return ResponseEntity.status(HttpStatus.OK).body(orderService.generateCheckoutLink());
   }
 
   @PostMapping("/callBackMono")
--- src/main/java/com/km220/service/OrderService.java
+++ src/main/java/com/km220/service/OrderService.java
@@ -1,8 +1,72 @@
 package com.km220.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
+  @Value("${monobank.token}")
+  private String monoToken;
+
+  public String generateCheckoutLink() throws IOException {
+    String result = null;
+
+    URL url = new URL("https://api.monobank.ua/api/merchant/invoice/create");
+    URLConnection con = url.openConnection();
+    HttpURLConnection http = (HttpURLConnection) con;
+    http.setRequestMethod("POST"); // PUT is another valid option
+    http.setDoOutput(true);
+    http.setRequestProperty("X-Token", monoToken);
+
+    String body = "{\n"
+        + "    \"amount\": 22000,\n"
+        + "    \"ccy\": 980,\n"
+        + "    \"merchantPaymInfo\": {\n"
+        + "        \"reference\": \"84d0070ee4e44667b31371d8f8813947\",\n"
+        + "        \"destination\": \"Покупка щастя\",\n"
+        + "        \"basketOrder\": []\n"
+        + "    },\n"
+        + "    \"redirectUrl\": \"https://example.com/your/website/result/page\",\n"
+        + "    \"webHookUrl\": \"https://example.com/mono/acquiring/webhook/maybesomegibberishuniquestringbutnotnecessarily\",\n"
+        + "    \"validity\": 3600,\n"
+        + "    \"paymentType\": \"debit\"\n"
+        + "}";
+
+    byte[] out = body.getBytes(StandardCharsets.UTF_8);
+    int length = out.length;
+
+    http.setFixedLengthStreamingMode(length);
+    http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+    http.connect();
+    try (OutputStream os = http.getOutputStream()) {
+      os.write(out);
+    }
+
+    BufferedReader br = null;
+    if (http.getResponseCode() == 200) {
+      br = new BufferedReader(new InputStreamReader(http.getInputStream()));
+      String strCurrentLine;
+      while ((strCurrentLine = br.readLine()) != null) {
+        System.out.println(strCurrentLine);
+        result = strCurrentLine;
+      }
+    } else {
+      br = new BufferedReader(new InputStreamReader(http.getErrorStream()));
+      String strCurrentLine;
+      while ((strCurrentLine = br.readLine()) != null) {
+        System.out.println(strCurrentLine);
+      }
+    }
+
+    return result;
+  }
 }
