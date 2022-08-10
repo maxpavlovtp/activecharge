f2a99cdd994fa8bb2e2ffd2c26d8742d3066714b
dimaborbotko <dimashechka0213@gmail.com>
2022-08-10 17:57:28 +0300

    220-146 changed dima sh

--- run-220-local-dima.sh
+++ run-220-local-dima.sh
@@ -14,8 +14,8 @@ nohup ./src-js/nest/run-nest-dev.sh &
 nohup ./src-js/react/run-react-local.sh &
 
 #BE
-#docker-compose down && rm -rf ./db-data
-nohup docker-compose up &
+#docker compose down && rm -rf ./db-data
+nohup docker compose up &
 ./gradlew clean build -x test
 echo "sleep for 10 secs..."
 sleep 10
