18b091acf89c1d2333807c3b4b6d07d211ecc787
dd <dimashechka0213@gmail.com>
2022-08-17 13:20:28 +0300

    220-152 back to div home card

--- src-js/react/src/components/stationCard/StationCard.tsx
+++ src-js/react/src/components/stationCard/StationCard.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import LoadingTime from "./LoadingTime";
-import "./StationCard.css";
+import styles from "./StationCard.module.css";
 import Timer from "../timer/Timer";
 import { useBackTime } from "../../hooks/useBackTime";
-import { Col, Row } from "react-bootstrap";
 
 export default function ({
   stationNumber,
@@ -31,49 +30,42 @@ export default function ({
     setHoursTime,
     setMinuteTime,
     setSecondsTime,
-    setLoading
+    setLoading,
   );
 
   return (
     <Link
       reloadDocument={true}
-      className="linkToStation"
+      className={styles.linkToStation}
       to={`/start?station=${stationNumber}`}
     >
-      <Row className="pl-4 pr-4 pt-4 pb-4 containerCard">
-        <Row className="mainInfo">
-          <Col as={"p"} className="nameStation">
-            Station
-          </Col>
-          <Col as={"p"} className="numberStation">
-            {stationNumber}
-          </Col>
-        </Row>
-        <Row className="align-items-end">
-          <Col lg={10}>
-            {state === "IN_PROGRESS" ? (
-              loading === true ? (
-                <LoadingTime />
-              ) : (
-                <Timer
-                  hours={hoursTime}
-                  minutes={minuteTime}
-                  seconds={secondsTime}
-                  margin={"20px 0 0 0"}
-                  fontSize={"20px"}
-                />
-              )
+      <div className={styles.container}>
+        <div className={styles.mainInfo}>
+          <p className={styles.nameStation}>Station</p>
+          <p className={styles.numberStation}>{stationNumber}</p>
+        </div>
+        <div className={styles.status}>
+          {state === "IN_PROGRESS" ? (
+            loading === true ? (
+              <LoadingTime />
             ) : (
-              <p className="readyCharge">Ready!</p>
-            )}
-          </Col>
-          <Col lg={2}>
-            <div
-              className={state === "IN_PROGRESS" ? "statusCard online" : "statusCard offline"}
-            ></div>
-          </Col>
-        </Row>
-      </Row>
+              <Timer
+                hours={1}
+                minutes={30}
+                seconds={0}
+                margin={"20px 0 0 0"}
+                fontSize={"30px"}
+              />
+            )
+          ) : (
+            <p className={styles.readyCharge}>Ready!</p>
+          )}
+
+          <div
+            className={state === "IN_PROGRESS" ? styles.online : styles.offline}
+          ></div>
+        </div>
+      </div>
     </Link>
   );
 }
