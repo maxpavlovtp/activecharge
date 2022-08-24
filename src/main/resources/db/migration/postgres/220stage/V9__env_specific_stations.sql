1ce54307bbb2ca3833541dc0c927bff1f5f3329d
max <mpavlov@mrcentertainment.com>
2022-08-24 14:17:54 +0300

    220-178 Db.Add envs config to flyway

---
+++ src/main/resources/db/migration/postgres/220stage/V9__env_specific_stations.sql
@@ -0,0 +1,3 @@
+truncate table station CASCADE;
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1-stage-komariv','1', '10013bb124');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('2-stage-tygranHome','2', '10015544ee');
---
+++ src/main/resources/db/migration/postgres/local/V9__env_specific_stations.sql
@@ -0,0 +1,2 @@
+truncate table station CASCADE;
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1-vlad-garage36','1', '1000d61c41');
