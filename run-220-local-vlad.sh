b7b0adf78e6f6089a42369fe552897373be81c1d
max <mpavlov@mrcentertainment.com>
2022-08-26 12:01:42 +0300

    220-1 nest clean up

--- run-220-local-vlad.sh
+++ run-220-local-vlad.sh
@@ -2,10 +2,8 @@ cd "$(dirname "$0")" || exit
 
 pkill -f java
 pkill -f react
-pkill -f nest
 
 lsof -ti tcp:8080 | xargs kill -kill
-lsof -ti tcp:5000 | xargs kill -kill
 lsof -ti tcp:3000 | xargs kill -kill
 
 #FE
