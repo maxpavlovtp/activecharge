f23238c76171a1c852b615f8e21e241d8e64b8ac
max <mpavlov@mrcentertainment.com>
2022-08-27 17:39:56 +0300

    220-143  Oauth login. js test

---
+++ src-js/ewilinkOauthTest.js
@@ -0,0 +1,26 @@
+// NodeJS
+const crypto = require('crypto');
+const clientId = 'xxx';
+const clientSecret = 'xxx';
+const seq = Date.now();
+const buffer = Buffer.from(`${clientId}_${seq}`, "utf-8");
+const sign = crypto.createHmac("sha256", clientSecret)
+.update(buffer)
+.digest(
+    "base64");
+console.log(sign);
+// v1+mfNY2ukxswM8sZOTg99srZsVnUVv9DGXeav1096M=
+
+let callBackTo220 = "http://49.12.19.42:8080/login/ewelinkRedirect";
+console.log(
+    "https://c2ccdn.coolkit.cc/oauth/index.html?"
+    + "state=220UserId"
+    + "&clientId=" + clientId
+    + "&authorization=" + sign
+    + "&seq=" + seq
+    + "&redirectUrl=" + callBackTo220
+    + "&nonce=zt123456"
+    + "&grantType=authorization_code"
+)
+
+
