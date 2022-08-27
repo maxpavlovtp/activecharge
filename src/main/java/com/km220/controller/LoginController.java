0881e9a0f4eec21697ca950b3c673a18d80cc3c5
max <mpavlov@mrcentertainment.com>
2022-08-27 16:55:57 +0300

    220-143  Oauth login. No strict rools

--- src/main/java/com/km220/controller/LoginController.java
+++ src/main/java/com/km220/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.km220.controller;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
   @GetMapping("/ewelinkRedirect")
-  public ResponseEntity<String> generateCheckoutLink(
-      @RequestParam("code") String code,
-      @RequestParam("regin") String region,
-      @RequestParam("state") String state
-  ) {
-    log.info("oAuth login call back from ewelink code: {}, region: {}, state: {}", code, region,
-        state);
+  public ResponseEntity<String> ewelinkRedirect(@RequestParam Map<String, String> allParams) {
+    log.info("oAuth login call back from ewelink is: {}", allParams.entrySet());
 
     return ResponseEntity.status(HttpStatus.OK).body(null);
   }
