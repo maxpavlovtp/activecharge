286d1953f6d6458964215a9bf681da938f84f84d
dd <dimashechka0213@gmail.com>
2022-09-02 16:46:17 +0300

    220-195 add plugShare map to home screen

--- src-js/react/src/App/homeScreen/HomeScreen.tsx
+++ src-js/react/src/App/homeScreen/HomeScreen.tsx
@@ -3,12 +3,15 @@ import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import ErrorPage from "../../components/error-page/ErrorPage";
+import Modal from "../../components/modal/Modal";
 import StationCard from "../../components/stationCard/StationCard";
 
 const urlV2StatusAll = `${process.env.REACT_APP_LINK_SERVE}device/v2/station/statusAll`;
 
 export default function HomeScreen() {
   const [statusALl, setStatusAll] = useState<any>();
+  const [modalActive, setModalActive] = useState(false);
+
   const { t } = useTranslation();
   useEffect(() => {
     try {
@@ -21,9 +24,9 @@ export default function HomeScreen() {
     }
   }, []);
   return (
-    <Container >
+    <Container>
       <Row className="justify-content-center">
-         {statusALl?.map((data: any) => {
+        {statusALl?.map((data: any) => {
           if (data?.state) {
             return (
               <Col lg={4} md={6} key={Math.random()}>
@@ -42,7 +45,35 @@ export default function HomeScreen() {
           }
         })}
       </Row>
-       
+      <Row className="justify-content-center">
+        <button
+          className="mt-5"
+          style={{
+            paddingTop: "5px",
+            paddingBottom: "5px",
+            paddingRight: "15px",
+            paddingLeft: "15px",
+            border: "none",
+            borderRadius: "15px",
+          }}
+          onClick={() => {
+            setModalActive(true);
+            console.log(modalActive);
+          }}
+        >
+          Map
+        </button>
+      </Row>
+      <Row className="justify-content-center">
+        <Modal active={modalActive} setActive={setModalActive}>
+          <iframe
+            src="https://www.plugshare.com/widget2.html?latitude=50.43307032970444&longitude=30.61636714055985&spanLat=0.002&spanLng=0.002&plugs=10"
+            width="800"
+            height="600"
+            allow="geolocation"
+          ></iframe>
+        </Modal>
+      </Row>
     </Container>
   );
 }
