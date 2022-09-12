dce7e26733b61fb8011efe3d428cfc0f40a1201c
tyhran <tygranmnatsakanian@gmail.com>
2022-09-13 00:20:05 +0300

    220-205 register all new devices to km220.com@gmail.com

---
+++ src/main/resources/db/migration/postgres/220stage/V14__new_devices_in_garage.sql
@@ -0,0 +1,7 @@
+INSERT INTO station ("name", "number", provider_device_id)VALUES('J1-local-Garage','3', '1001690629');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('J2-local-Garage','4', '1001692538');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('J3-local-Garage','5', '1001692662');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('G1-local-Garage','6', '100169061d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('G2-local-Garage','7', '100169082d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('G3-local-Garage','8', '10016905cf');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('G4-local-Garage','9', '100169010c');
