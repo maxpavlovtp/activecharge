4560ecbadc35194b55ae1b76db01523f72c506d5
tyhran <tygranmnatsakanian@gmail.com>
2022-09-13 11:43:17 +0300

    220-205 register all new devices to km220.com@gmail.com

--- src/main/resources/db/migration/pg/V8__add_tyhran_device.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO station ("name", "number", provider_device_id)VALUES('tyhran','1001', '10015544ee');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('tyhran-1','1001', '1000d61c41');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('tyhran-2','1002', '1001323420');
-
--- src/main/resources/db/migration/postgres/220stage/V14__new_devices_in_garage.sql
+++ src/main/resources/db/migration/postgres/220stage/V14__new_devices_in_garage.sql
@@ -1,7 +1,7 @@
-INSERT INTO station ("name", "number", provider_device_id)VALUES('J1-local-Garage','3', '1001690629');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('J2-local-Garage','4', '1001692538');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('J3-local-Garage','5', '1001692662');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('G1-local-Garage','6', '100169061d');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('G2-local-Garage','7', '100169082d');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('G3-local-Garage','8', '10016905cf');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('G4-local-Garage','9', '100169010c');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('3-stage-Garage36','3', '1001690629');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('4-stage-Garage36','4', '1001692538');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('5-stage-Garage36','5', '1001692662');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('6-stage-Garage36','6', '100169061d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('7-stage-Garage36','7', '100169082d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('8-stage-Garage36','8', '10016905cf');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('9-stage-Garage36','9', '100169010c');
