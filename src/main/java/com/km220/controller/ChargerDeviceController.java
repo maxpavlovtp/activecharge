77786a4b247c99d6f598fdd831d51b18701becb7
Maks Pavlov <eskimorollerr@gmail.com>
2022-09-19 21:02:54 +0300

    220-1 one station on prod

--- src/main/java/com/km220/controller/ChargerDeviceController.java
+++ src/main/java/com/km220/controller/ChargerDeviceController.java
@@ -17,19 +17,22 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.NotBlank;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/device")
@@ -50,7 +53,7 @@ public class ChargerDeviceController {
 	@PostMapping("/v2/start")
 	public ResponseEntity<CreatedChargingJob> start(
 			@Parameter(description = "Charge request parameters") @RequestBody ChargeRequest chargeRequest) {
-		int chargePeriodInSeconds = this.freeChargeSecs;
+		int chargePeriodInSeconds = chargeRequest.getChargePeriodInSeconds();
 
 		UUID id = chargerService.start(chargeRequest.getStationNumber(),
 				chargePeriodInSeconds);
