ecf756c317873da1e88f20115e49d3b15b57b8ba
Maks Pavlov <eskimorollerr@gmail.com>
2022-09-19 21:01:11 +0300

    220-1 one station on prod

--- src/main/java/com/km220/controller/ChargerDeviceController.java
+++ src/main/java/com/km220/controller/ChargerDeviceController.java
@@ -18,7 +18,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,90 +36,95 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ChargerDeviceController {
 
-  @Value("${device.freeChargeSecs}")
-  private int freeChargeSecs;
+	private final StationScanProperties stationScanProperties;
+	private final ChargerService chargerService;
+	private final ChargingJobService chargingJobService;
+	private final GPSService gpsService;
 
-  private final StationScanProperties stationScanProperties;
-  private final ChargerService chargerService;
-  private final ChargingJobService chargingJobService;
-  private final GPSService gpsService;
+	@Operation(summary = "Start charging")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Created charging job",
+					content = {@Content(mediaType = "application/json",
+							schema = @Schema(implementation = CreatedChargingJob.class))})
+	})
+	@PostMapping("/v2/start")
+	public ResponseEntity<CreatedChargingJob> start(
+			@Parameter(description = "Charge request parameters") @RequestBody ChargeRequest chargeRequest) {
+		int chargePeriodInSeconds = this.freeChargeSecs;
 
-  @Operation(summary = "Start charging")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "201", description = "Created charging job",
-          content = {@Content(mediaType = "application/json",
-              schema = @Schema(implementation = CreatedChargingJob.class))})
-  })
-  @PostMapping("/v2/start")
-  public ResponseEntity<CreatedChargingJob> start(
-      @Parameter(description = "Charge request parameters") @RequestBody ChargeRequest chargeRequest) {
-    int chargePeriodInSeconds = this.freeChargeSecs;
+		UUID id = chargerService.start(chargeRequest.getStationNumber(),
+				chargePeriodInSeconds);
+		return ResponseEntity.status(HttpStatus.CREATED).body(CreatedChargingJob.builder()
+				.id(id.toString())
+				.scanIntervalMs(stationScanProperties.getScanIntervalMs())
+				.build()
+		);
+	}
 
-    UUID id = chargerService.start(chargeRequest.getStationNumber(),
-        chargePeriodInSeconds);
-    return ResponseEntity.status(HttpStatus.CREATED).body(CreatedChargingJob.builder()
-        .id(id.toString())
-        .scanIntervalMs(stationScanProperties.getScanIntervalMs())
-        .build()
-    );
-  }
+	@Operation(summary = "Get charging status by id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Charging job status",
+					content = {@Content(mediaType = "application/json",
+							schema = @Schema(implementation = ChargingJob.class))})
+	})
+	@GetMapping("/v2/status")
+	public ResponseEntity<ChargingJob> getStatus(
+			@Parameter(description = "Charging job id") @NotBlank @RequestParam String id) {
 
-  @Operation(summary = "Get charging status by id")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Charging job status",
-          content = {@Content(mediaType = "application/json",
-              schema = @Schema(implementation = ChargingJob.class))})
-  })
-  @GetMapping("/v2/status")
-  public ResponseEntity<ChargingJob> getStatus(
-      @Parameter(description = "Charging job id") @NotBlank @RequestParam String id) {
+		ChargingJobEntity job = chargingJobService.findByJobId(id);
+		if (job == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(ChargingJobConverter.INSTANCE.apply(job));
+	}
 
-    ChargingJobEntity job = chargingJobService.findByJobId(id);
-    if (job == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-    }
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(ChargingJobConverter.INSTANCE.apply(job));
-  }
+	@Operation(summary = "Get charging status by station number")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Charging job status",
+					content = {@Content(mediaType = "application/json",
+							schema = @Schema(implementation = ChargingJob.class))})
+	})
+	@GetMapping("/v2/station/status")
+	public ResponseEntity<StationState> getStationStatus(
+			@Parameter(description = "Station number") @NotBlank @RequestParam("station_number") String stationNumber) {
 
-  @Operation(summary = "Get charging status by station number")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Charging job status",
-          content = {@Content(mediaType = "application/json",
-              schema = @Schema(implementation = ChargingJob.class))})
-  })
-  @GetMapping("/v2/station/status")
-  public ResponseEntity<StationState> getStationStatus(
-      @Parameter(description = "Station number") @NotBlank @RequestParam("station_number") String stationNumber) {
+		ChargingJobEntity job = chargingJobService.findByStationNumber(stationNumber);
+		StationState stationState = StationStateConverter.INSTANCE.apply(job);
+		//TODO: refactor
+		if (stationState.getLastJob() != null) {
+			stationState.getLastJob().setUiNightMode(gpsService.getUiNightMode());
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(stationState);
+	}
 
-    ChargingJobEntity job = chargingJobService.findByStationNumber(stationNumber);
-    StationState stationState = StationStateConverter.INSTANCE.apply(job);
-    //TODO: refactor
-    if (stationState.getLastJob() != null) {
-      stationState.getLastJob().setUiNightMode(gpsService.getUiNightMode());
-    }
-    return ResponseEntity.status(HttpStatus.OK).body(stationState);
-  }
 
-  @GetMapping("/v2/station/statusAll")
-  public ResponseEntity<List<ChargingJob>> getStatusAll() {
-    String[] stations = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	//todo remove
+	@Autowired
+	Environment env;
 
-    // todo implemtent
-    List<ChargingJob> jobs = chargingJobService.getInProgressJobs();
+	@GetMapping("/v/station/statusAll")
+	public ResponseEntity<List<ChargingJob>> getStatusAll() {
 
-    jobs = Arrays.stream(stations)
-        .map(id -> new ChargingJob(id, System.currentTimeMillis(), 3600))
-        .map(job -> {
-          if ("1".equals(job.getStationNumber())) {
-            job.setState(ChargingJobState.DONE);
-          } else {
-            job.setState(ChargingJobState.IN_PROGRESS);
-          }
-          return job;
-        })
-        .collect(Collectors.toList());
+		String[] stations = Arrays.stream(env.getActiveProfiles()).toList().contains("prod") ?
+				new String[]{"1"} :
+				new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-    return ResponseEntity.status(HttpStatus.OK).body(jobs);
-  }
+		// todo implemtent
+		List<ChargingJob> jobs = chargingJobService.getInProgressJobs();
+
+		jobs = Arrays.stream(stations)
+				.map(id -> new ChargingJob(id, System.currentTimeMillis(), 3600))
+				.map(job -> {
+					if ("1".equals(job.getStationNumber())) {
+						job.setState(ChargingJobState.DONE);
+					} else {
+						job.setState(ChargingJobState.IN_PROGRESS);
+					}
+					return job;
+				})
+				.collect(Collectors.toList());
+
+		return ResponseEntity.status(HttpStatus.OK).body(jobs);
+	}
 }
