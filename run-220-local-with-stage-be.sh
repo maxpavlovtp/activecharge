4bff818f0b3d1d0eb5b9a601f8c8d88a6b740f18
dd <dimashechka0213@gmail.com>
2022-09-19 12:36:47 +0300

    220-228 add sh for start only local and start local stage BE

--- run-220-local-with-stage-be.sh
+++ run-220-local-with-stage-be.sh
@@ -1,10 +1,7 @@
 cd "$(dirname "$0")" || exit
-  #"http://localhost:8080/"
-
 
 pkill -f react
 lsof -ti tcp:3000 | xargs kill -kill
 
 #FE
-#todo add new
-nohup ./src-js/react/run-react-local.sh &
+nohup ./src-js/react/run-react-localLimited.sh &
