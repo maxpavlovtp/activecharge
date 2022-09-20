7105cf5e234537c92cc7a78908b97989c7972cdb
Maks Pavlov <eskimorollerr@gmail.com>
2022-09-20 22:24:38 +0300

    220-143  Oauth login for ewelink. Check

--- src/main/java/com/km220/ewelink/internal/model/CredentialsResponse.java
+++ src/main/java/com/km220/ewelink/internal/model/CredentialsResponse.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.km220.ewelink.internal.login.User;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
+import org.springframework.beans.factory.annotation.Value;
 
 @Jacksonized
 @Builder
@@ -17,12 +19,22 @@ import lombok.extern.jackson.Jacksonized;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CredentialsResponse {
 
-  private String at;
-  private String rt;
-  private User user;
-  private String region;
-  private int error;
-  private String msg;
-  @Default
-  private Map<String, Object> additionalProperties = new HashMap<>();
+	@Value("${ewelink.useOAuth}")
+	private boolean useOAuth;
+	@Value("${ewelink.oAuth-token}")
+	private String oAuthToken;
+
+	public String getAt() {
+		return useOAuth ? oAuthToken : at;
+	}
+
+	@Getter(AccessLevel.NONE)
+	private String at;
+	private String rt;
+	private User user;
+	private String region;
+	private int error;
+	private String msg;
+	@Default
+	private Map<String, Object> additionalProperties = new HashMap<>();
 }
