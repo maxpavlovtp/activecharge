bc86a3a4ef83f465db527653d79fe77c0e1f28bf
Maks Pavlov <eskimorollerr@gmail.com>
2022-09-20 22:31:21 +0300

    220-143  Oauth login for ewelink. Check

--- src/main/java/com/km220/ewelink/internal/model/CredentialsResponse.java
+++ src/main/java/com/km220/ewelink/internal/model/CredentialsResponse.java
@@ -6,12 +6,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.km220.ewelink.internal.login.User;
 import java.util.HashMap;
 import java.util.Map;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
-import org.springframework.beans.factory.annotation.Value;
 
 @Jacksonized
 @Builder
@@ -19,16 +17,6 @@ import org.springframework.beans.factory.annotation.Value;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CredentialsResponse {
 
-	@Value("${ewelink.useOAuth}")
-	private boolean useOAuth;
-	@Value("${ewelink.oAuth-token}")
-	private String oAuthToken;
-
-	public String getAt() {
-		return useOAuth ? oAuthToken : at;
-	}
-
-	@Getter(AccessLevel.NONE)
 	private String at;
 	private String rt;
 	private User user;
