ca4a816d4091ea398be559864571410986503e0c
dd <dimashechka0213@gmail.com>
2022-09-22 13:03:50 +0300

    220-191 generate link when click

--- src-js/react/src/App/mainScreen/MainSection.tsx
+++ src-js/react/src/App/mainScreen/MainSection.tsx
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./MainSection.css";
 
 import { Link, useOutletContext, useSearchParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
-import { getStationInfo, idStart } from "../../store/reducers/ActionCreators";
+import { idStart } from "../../store/reducers/ActionCreators";
 import MainImgLoadingLazy from "../../components/lazyLoading/MainImgLoadingLazy";
 import placehoderSrc from "../../assets/chargingTiny.png";
 import ErrorPage from "../../components/error-page/ErrorPage";
-// import { setDeviceStatusUndefind } from "../../store/reducers/FetchSlice";
 import axios from "axios";
 import { Col, Container, Row } from "react-bootstrap";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
-import { MainScreenLink } from "../../components/globalStyles";
 import { setDeviceStatusUndefind } from "../../store/reducers/FetchSlice";
+import { PayLinkLoading } from "../../components/stationCard/LoadingTime";
 
 const MainSection: React.FC = () => {
   const [searchParams] = useSearchParams();
-  const [payUrls, setPayUrls] = useState<any>([]);
   const [errorPay, setErrorPay] = useState<any>(null);
   const [mainImgTheme] = useOutletContext<any>();
+
+  const [loadingSixPayLink, setLoadingSixPayLink] = useState(false);
+  const [loadingTwelvePayLink, setLoadingTwelvePayLink] = useState(false);
 
   let stationNumber: any = searchParams.get("station");
   const urlPayment12h = `${process.env.REACT_APP_LINK_SERVE}order/generateCheckoutLink?station_number=${stationNumber}&&hours=12`;
   const urlPayment6h = `${process.env.REACT_APP_LINK_SERVE}order/generateCheckoutLink?station_number=${stationNumber}&&hours=6`;
-  const payEndpoints = [urlPayment6h, urlPayment12h];
 
   const { t } = useTranslation();
 
@@ -38,26 +37,30 @@ const MainSection: React.FC = () => {
   };
   // http://49.12.19.42:8080/
   // http://localhost:8080/
-  useEffect(() => {
+
+  const goPaySix = () => {
+    setLoadingSixPayLink(true);
     try {
-      if (process.env.REACT_APP_LINK_SERVE === "http://localhost:8080/") {
-        console.log("local dev");
-      } else {
-        axios
-          .all(payEndpoints.map((endpoint: any) => axios.get(endpoint)))
-          .then((data) => {
-            setPayUrls([]);
-            data?.map((link: any) => {
-              const { pageUrl } = link.data;
-              setPayUrls((pay: any) => [...pay, pageUrl]);
-              console.log(pageUrl);
-            });
-          });
-      }
+      axios.get(urlPayment6h).then((link) => {
+        window.open(link.data.pageUrl, "_blank");
+        setLoadingSixPayLink(false);
+      });
     } catch (e: any) {
       setErrorPay(e.message);
     }
-  }, []);
+  };
+
+  const goPayTwelve = () => {
+    setLoadingTwelvePayLink(true);
+    try {
+      axios.get(urlPayment12h).then((link) => {
+        window.open(link.data.pageUrl, "_blank");
+        setLoadingTwelvePayLink(false);
+      });
+    } catch (e: any) {
+      setErrorPay(e.message);
+    }
+  };
 
   let statusBtn = errorPay !== null ? "btnStart disableBtn" : "btnStart";
 
@@ -81,7 +84,7 @@ const MainSection: React.FC = () => {
       </Row>
       <Row className="justify-content-center mt-2 mb-5">
         <Col
-          xs="auto"
+          xs="3"
           as={Link}
           to={`/charging?station=${stationNumber}`}
           className="btnStart"
@@ -92,23 +95,31 @@ const MainSection: React.FC = () => {
 
         <Col
           as={"a"}
-          xs="auto"
+          xs="3"
           className={`ml-2 ${statusBtn}`}
-          href={`${payUrls[0]}`}
+          onClick={goPaySix}
           target="_blank"
           rel="noreferrer"
         >
-          6{t("btns.start")}
+          {loadingSixPayLink === true ? (
+            <PayLinkLoading />
+          ) : (
+            `6${t("btns.start")}`
+          )}
         </Col>
         <Col
           as={"a"}
-          xs="auto"
+          xs="3"
           className={`ml-2 ${statusBtn}`}
-          href={`${payUrls[1]}`}
+          onClick={goPayTwelve}
           target="_blank"
           rel="noreferrer"
         >
-          12{t("btns.start")}
+          {loadingTwelvePayLink === true ? (
+            <PayLinkLoading />
+          ) : (
+            `12${t("btns.start")}`
+          )}
         </Col>
       </Row>
 
