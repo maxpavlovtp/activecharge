2a0b5b97a905707e373fd67522b1b5cb97f51777
Maks Pavlov <eskimorollerr@gmail.com>
2022-10-05 19:34:12 +0300

    220-260 bug. Endless charging

--- src/main/resources/db/migration/postgres/220stage/V16__delete_not_compatible_devices.sql
+++ src/main/resources/db/migration/postgres/220stage/V16__delete_not_compatible_devices.sql
@@ -1,8 +1,10 @@
-DELETE
-FROM public.station
-WHERE id = 'c1b69eb7-dec4-4cd6-8d92-04a1b8e4b43b';
-
-DELETE
-FROM public.station
-WHERE id = 'ffced0bd-5a66-4020-adc0-1bd6e43892ae';
+truncate table station CASCADE;
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1-stage-komariv','1',    '10013bb124');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('2-stage-tygranHome','2', '10015544ee');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('3-stage-Garage36','3',   '100169061d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('4-stage-Garage36','4',   '100169082d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('5-stage-Garage36','5',   '10016905cf');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('6-stage-Garage36','6',   '100169010c');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('7-stage-Garage36','7',   '1001692662');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('8-always-offline','8',   '100136f129');
 
