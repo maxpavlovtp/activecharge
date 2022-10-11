eceec31b9a0c36e45495045fc3012eddcdb041a0
Maks Pavlov <eskimorollerr@gmail.com>
2022-10-11 14:13:54 +0300

    220-242 Show "device offline" error instead start page when device offline

--- src/main/java/com/km220/service/StationService.java
+++ src/main/java/com/km220/service/StationService.java
@@ -2,19 +2,27 @@ package com.km220.service;
 
 import com.km220.dao.station.StationEntity;
 import com.km220.dao.station.StationRepository;
+import com.km220.service.device.DeviceService;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StationService {
 
-  private final StationRepository stationRepository;
+	private final StationRepository stationRepository;
+	private final DeviceService deviceService;
 
-  public StationService(final StationRepository stationRepository) {
-    this.stationRepository = stationRepository;
-  }
+	public StationService(final StationRepository stationRepository, DeviceService deviceService) {
+		this.stationRepository = stationRepository;
+		this.deviceService = deviceService;
+	}
 
-  public List<StationEntity> getAllStations() {
-    return stationRepository.findAll();
-  }
+	public List<StationEntity> getAllStations() {
+		return stationRepository.findAll();
+	}
+
+	public boolean isOnline(String station) {
+		return deviceService.getState(stationRepository.getByNumber(station).getDeviceId())
+				.getVoltage() > 0;
+	}
 }
