083c57826b92a1af5002bdfe207b6973e4d3fa77
Maks Pavlov <eskimorollerr@gmail.com>
2022-10-19 23:13:11 +0300

    220-1 + 6 deices on stage

---
+++ src/main/resources/db/migration/postgres/220stage/V19__add_6_stations.sql
@@ -0,0 +1,16 @@
+truncate table station CASCADE;
+
+INSERT INTO station ("name", "number", provider_device_id)VALUES('10013bb124', '1','10013bb124');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('10015544ee', '2','10015544ee');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('100169061d', '3','100169061d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('100169082d', '4','100169082d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('10016905cf', '5','10016905cf');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('100169010c', '6','100169010c');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1001692662', '7','1001692662');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('100155240d', '8','100155240d');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('10016935ac', '9','10016935ac');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1001693a00', '10','1001693a00');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('10016919c9', '11','10016919c9');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1001692457', '12','1001692457');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1001694660', '13','1001694660');
+
