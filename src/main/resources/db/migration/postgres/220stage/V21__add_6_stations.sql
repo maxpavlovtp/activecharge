6b80045ec37a693cd754a2bb004daffaf28b1fe6
Maks Pavlov <eskimorollerr@gmail.com>
2022-10-23 14:06:28 +0300

    220-1 metrics

--- src/main/resources/db/migration/postgres/220stage/V21__add_6_stations.sql
+++ src/main/resources/db/migration/postgres/220stage/V21__add_6_stations.sql
@@ -6,7 +6,6 @@ INSERT INTO station ("name", "number", provider_device_id)VALUES('3', '3', '1001
 INSERT INTO station ("name", "number", provider_device_id)VALUES('4', '4', '10016905cf');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('5', '5', '100169010c');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('6', '6', '1001692662');
-INSERT INTO station ("name", "number", provider_device_id)VALUES('7', '7', '100155240d');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('8', '8', '10016935ac');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('9', '9', '1001693a00');
 INSERT INTO station ("name", "number", provider_device_id)VALUES('10', '10','10016919c9');
--- src/main/resources/db/migration/postgres/vlad/V9__add_dedicated_device_for_vlad.sql
+++ src/main/resources/db/migration/postgres/vlad/V9__add_dedicated_device_for_vlad.sql
@@ -1,2 +1,3 @@
 truncate table station CASCADE;
-INSERT INTO station ("name", "number", provider_device_id)VALUES('1-vlad-alwaysWithDima','1', '10013bd071');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('1-vlad-dima-komariv','1', '10013bd071');
+INSERT INTO station ("name", "number", provider_device_id)VALUES('2-vlad-max-kyiv', '2', '100155240d');
